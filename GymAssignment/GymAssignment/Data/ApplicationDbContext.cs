57bb3c79706ff2ebc514ad2caecbc9c36872746e
Tyler <200382154@student.georgianc.on.ca>
2019-11-25 13:21:41 -0500

    Adding in Users Table
    Adding the users table and setting up the correct views

--- GymAssignment/GymAssignment/Data/ApplicationDbContext.cs
+++ GymAssignment/GymAssignment/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using GymAssignment.Models;
 
 namespace GymAssignment.Data
 {
@@ -12,5 +13,6 @@ namespace GymAssignment.Data
             : base(options)
         {
         }
+        public DbSet<GymAssignment.Models.UsersTable> UsersTable { get; set; }
     }
 }
