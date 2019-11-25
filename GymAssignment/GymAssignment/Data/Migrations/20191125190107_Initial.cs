69e7745c9cc4b2054353f56614164b91a61279d7
Tyler <200382154@student.georgianc.on.ca>
2019-11-25 14:03:27 -0500

    Reverting Changes
    Updating, adding the database correctly

---
+++ GymAssignment/GymAssignment/Data/Migrations/20191125190107_Initial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GymAssignment.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UsersTable",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    Address = table.Column<string>(maxLength: 255, nullable: false),
+                    PhoneNumber = table.Column<string>(maxLength: 20, nullable: false),
+                    StartingWeight = table.Column<decimal>(nullable: false),
+                    GoalWeight = table.Column<decimal>(nullable: false),
+                    Location = table.Column<string>(maxLength: 100, nullable: false),
+                    PreferredTraining = table.Column<string>(maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UsersTable", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UsersTable");
+        }
+    }
+}
---
+++ GymAssignment/GymAssignment/Models/UsersTable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GymAssignment.Models
+{
+    public class UsersTable
+    {
+        [Key]                               //Making a primary key to store all the users in our database.
+        public virtual int Id { get; set; }
+        [Required]
+        [RegularExpression(@"^[A-Z]+[a-zA-z""'\s-]*$")]
+        public virtual String Name { get; set; }
+        [Required]
+        [StringLength(255)]
+        public virtual String Address { get; set; }
+        [Required]
+        [StringLength(20)]
+        public virtual String PhoneNumber { get; set; }
+        public virtual Decimal StartingWeight { get; set; } //Not Required, only if User wants to opt-in with this information, decimal so no rounding errors
+        public virtual Decimal GoalWeight { get; set; } //Not Required, only if User wants to opt-in with this information
+        [Required]
+        [StringLength(100)]
+        public virtual String Location { get; set; }
+        [StringLength(50)]                  //Not Required, only if User wants to opt-in with this information
+        public virtual String PreferredTraining { get; set; }
+    }
+}
