ca03c0bec04ea26db6d32bfb8e2d973d85316f47
Tyler <200382154@student.georgianc.on.ca>
2019-12-11 02:52:55 -0500

    Adding in Unit Testing
    Basic unit testing

---
+++ GymAssignment/GymAssignmentTests/HomeTesting.cs
@@ -0,0 +1,27 @@
+using System;
+using Xunit;
+using Microsoft.EntityFrameworkCore;
+using GymAssignment.Controllers;
+using System.Data.Entity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymAssignmentTests
+{
+
+    public class HomeTesting
+    {
+
+
+        [Fact]
+        public void IndexTest()
+        {
+
+            //Arrange
+            //HomeController controller = new HomeController();
+            //Act
+            //ViewResult result = controller.Index() as ViewResult();
+            //Assert
+            //Assert.NotNull(result);
+        }
+    }
+}
